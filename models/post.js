2a36477462d74825a24ae80564c2cd7c6a3e70ad
mourad132 <58851960+mourad132@users.noreply.github.com>
2020-08-06 14:01:58 +0200

    Update post.js

--- models/post.js
+++ models/post.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 var schema = new mongoose.Schema({
     title: String,
     post: String,
+	author: String,
 });
 
 module.exports = mongoose.model("Post", schema);
