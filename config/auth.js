bbe1a5cb95e6a9603d335914ddde2b5ba6f591be
mourad132 <58851960+mourad132@users.noreply.github.com>
2020-08-11 16:32:41 +0200

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,23 +1,22 @@
 module.exports = {
-    ensureAuthenticated: function(req, res, next) {
-      if (req.isAuthenticated()) {
-        return next();
-      }
-      req.flash('error_msg', 'Please log in to view that resource');
-      res.redirect('/users/login');
-    },
-    forwardAuthenticated: function(req, res, next) {
-      if (!req.isAuthenticated()) {
-        return next();
-      }
-      res.redirect('/home');      
-    },
-      isDev: function(req, res, next){
-          if(req.user._id == "5f2ed45a2625c21c561e3a21"){
-              return next()
-          } else {
-              res.redirect("/home")
-          }
-      }	
-      };
-  
+  ensureAuthenticated: function(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.flash('error_msg', 'Please log in to view that resource');
+    res.redirect('/users/login');
+  },
+  forwardAuthenticated: function(req, res, next) {
+    if (!req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/home');      
+  },
+	isDev: function(req, res, next){
+		if(req.user._id == "5f32a85f49d2e017c66b3608"){
+			return next()
+		} else {
+			res.redirect("/home")
+		}
+	}	
+	};
