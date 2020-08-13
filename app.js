5a680ce94b6ef8235696d98e6f6f0fd100ca4400
mourad132 <58851960+mourad132@users.noreply.github.com>
2020-08-13 15:32:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -265,7 +265,7 @@ app.get("/profilephoto", (req, res) => {
 	res.render("profilephoto")
 })
 
-app.get("/photo", ensureAuthenticated, function(req, res){
+app.get("/photo", function(req, res){
 	res.render("photo")
 })
 
