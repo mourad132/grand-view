bc59ff4fa674ee34448958002683045350a7675a
mourad132 <58851960+mourad132@users.noreply.github.com>
2020-08-23 22:25:22 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -33,6 +33,10 @@ const UserSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	registeration: {
+		type: String,
+		required: true,
+	}
   date: {
     type: Date,
     default: Date.now
