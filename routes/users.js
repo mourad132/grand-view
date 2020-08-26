08673bd4adef144a41adb460fd0a60e18256d051
mourad132 <58851960+mourad132@users.noreply.github.com>
2020-08-26 18:15:18 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -90,7 +90,7 @@ router.post('/register', (req, res) => {
 // Login
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', {
-    successRedirect: '/home,
+    successRedirect: '/home',
     failureRedirect: '/users/login',
     failureFlash: true
   })(req, res, next);
